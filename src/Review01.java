8fb1e482acbcab3ae61d638d2348bb91a9b3e38d
kenchanta <exnowman@gmail.com>
2025-02-09 22:03:26 +0900

    「double taxPercentage = 0.1;」を、tax関数の引数に指定するのではなく、tax関数に入れ込んだ。

--- src/Review01.java
+++ src/Review01.java
@@ -2,17 +2,18 @@
 public class Review01 {
 
     public static void main(String[] args) {
+        //商品価格
         int origin = 1500;
-        double taxPercentage = 0.1;
-
-        int taxFee = tax(origin,taxPercentage);
+        //消費税額
+        int taxFee = tax(origin);
+        //税込み価格
         int taxInclude = origin + taxFee;
-
         System.out.println(origin +"円の商品の税込価格は"+taxInclude+"円"+"(消費税は"+taxFee+"円）です。");
     }
 
     //消費税額（商品価格×税率）の計算
-    public static int tax(int origin, double taxPercentage) {
+    public static int tax(int origin) {
+        double taxPercentage = 0.1;
         int taxFee = (int) (origin * taxPercentage);
         return taxFee;
     }
